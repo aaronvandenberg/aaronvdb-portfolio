e87618b466750bddb99624ae94fc44bd788c1c6f
aaronvandenberg <avdbergbusiness@gmail.com>
2023-02-11 18:13:03 +0200

    Fixed WinBox not working

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const about = document.querySelector('#about')
+const poortfolio = document.querySelector('#portfolio')
+const aboutContent = document.querySelector
+('#about-content')
+const contactContent = document.querySelector
+('#contact-content')
+
+about.addEventListener('click', () => {
+  new WinBox()
+})
