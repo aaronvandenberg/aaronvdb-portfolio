8c017f865cb2fec34ca1d41c89f4a155609595d3
aaronvandenberg <avdbergbusiness@gmail.com>
2023-02-11 19:47:55 +0200

    Slowly everything is coming together

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
 const about = document.querySelector('#about')
-const poortfolio = document.querySelector('#portfolio')
-const aboutContent = document.querySelector
-('#about-content')
-const contactContent = document.querySelector
-('#contact-content')
+const portfolio = document.querySelector('#portfolio')
+const aboutContent = document.querySelector('#about-content')
+const contactContent = document.querySelector('#contact-content')
+const portfolioContent = document.querySelector('#portfolio-content')
 
 about.addEventListener('click', () => {
-  new WinBox()
+    const aboutBox = new WinBox({
+    title: 'About me',
+    background: '#00E4FF',
+    width: '500px',
+    height:'500px',
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: aboutContent,
+    })
+})
+
+portfolio.addEventListener('click', () => {
+    const portfolioBox = new WinBox({
+    title: 'Projects',
+    background: '#00E4FF',
+    width: '500px',
+    height:'500px',
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: portfolioContent,
+    })
 })
