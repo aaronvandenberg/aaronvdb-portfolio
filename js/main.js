e1ce7736bea2a39c4c1656e91604c458e5ca5743
aaronvandenberg <avdbergbusiness@gmail.com>
2023-02-14 02:35:40 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 const about = document.querySelector('#about')
 const portfolio = document.querySelector('#portfolio')
+const contact = document.querySelector('#contact')
 const aboutContent = document.querySelector('#about-content')
 const contactContent = document.querySelector('#contact-content')
 const portfolioContent = document.querySelector('#portfolio-content')
+
+
+
+console.log("iVGhpcyBpcyB0aGUga2V5IHRvIEFhcm9uJ3MgbWFpbmZyYW1lLCB1c2UgaXQgd2lzZWx5IQ==")
 
 about.addEventListener('click', () => {
     const aboutBox = new WinBox({
@@ -15,6 +20,8 @@ about.addEventListener('click', () => {
     bottom: 50,
     left: 50,
     mount: aboutContent,
+	    
+
     })
 })
 
@@ -30,4 +37,24 @@ portfolio.addEventListener('click', () => {
     left: 50,
     mount: portfolioContent,
     })
+})
+
+
+
+contact.addEventListerner('click', () => {
+	const contactBox = new Winbox({
+		title: 'Contact',
+		background: '#00E4FF',
+		width: '500px',
+		height: '500px',
+		top: 50,
+		right: 50,
+		bottom: 50,
+		left: 50,
+		mount contactContent,
+
+
+	})
+
+
 })
